304816a0c351556aa3074115c4de622ca373d6f9
mohammad <ekhlasimohammad345@gmail.com>
2025-04-30 21:46:24 +0200

    changes on to do app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 const initialToDos=[
   {
     id: 1 ,
@@ -23,15 +25,35 @@ const initialToDos=[
 
 function App() {
 
+
+  const [newTask,setNewTask]=useState("") 
+  const [allTodos,setAllTodos]=useState(initialToDos) 
+
+
+
+  function submitNewTodo(e){
+    e.preventDefault()
+    if (newTask !==""){
+      let newTodos= allTodos.concat({
+        id: allTodos.length + 1,
+        task: newTask,
+        completed: false,
+      })
+      
+      setAllTodos(newTodos)
+      setNewTask("")
+    } 
+  }
+
   return (
     <>
       <h1>My To do List</h1>
-      <form>
-        <input type="text" placeHolder="write here..."></input>
+      <form onSubmit={submitNewTodo}>
+        <input type="text" placeHolder="write here..." value={newTask} onChange={e => setNewTask(e.target.value) }></input>
         <button type="submit">submit</button>
       </form>
       <ul>
-      {initialToDos.map(todo =>(
+      {allTodos.map(todo =>(
         <li>
           {todo.task} 
         </li>
