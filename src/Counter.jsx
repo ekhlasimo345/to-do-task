85057add6a84292ea6f00098718f2356eb01626a
mohammad <ekhlasimohammad345@gmail.com>
2025-05-07 17:59:51 +0200

    counter changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -9,11 +9,11 @@ function Counter({label}){
         <h2>{label}</h2>
         <button onClick={e => setCount(count +1 )}> + </button>   
         <span>{count}</span>
-        <button onClick={e => setCount(count -1)}> - </button>   
-        
+        {/* <button style={{cursor : count ===0 ? "not-allowed" :"pointer" }}  disabled={count ===0} onClick={e => setCount(count -1)}> - </button>    */}
+        <button className={count ===0 ? 'button-disabled': 'button-active'} disabled={count ===0} onClick={e => setCount(count -1)}> - </button>   
 
     </div>
   )
 }
-
+console.log("hello world")
 export default Counter
